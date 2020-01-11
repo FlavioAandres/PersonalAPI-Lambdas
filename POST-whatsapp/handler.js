c4ce2208713279d34ffc94d2415afab12d7c0c70
FlavioAAndres <fpareja@condorlabs.io>
2020-01-11 08:52:53 -0500

    sum and substract cateogries

--- POST-whatsapp/handler.js
+++ POST-whatsapp/handler.js
@@ -1,23 +1,46 @@
 'use strict';
+require('dotenv').config()
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('./src/utils/bodyParser')
 const cashController = require('./src/controllers/finanzasController')
 
-module.exports.handler = async (event, context) => {
-  console.log(JSON.stringify(event))
-  try {
 
-    const object = bodyParser(event.body)
-    const twiml = new MessagingResponse();
-    if (object.Body.indexOf('--help') >= 0) {
-      twiml.message(`Wellcome to PersonalAPI. Formats about what you wanna do: 🤘🤘`)
-      twiml.message(`
-      *SaveFinanzas:*\nCash + CATEGORY + AMOUNT + DESCRIPTION\nexample: "cash Comida 12000 Almuerzo"\n\n*Total Category:* Total + CATEGORY + DATE_AGO\nExample: "Total Transporte 5week"formats: 1week, 1month, 1Year`)
-      twiml.message(`*Reminders:* \nReminder + DESCRIPTION \n example: "Reminder Llamar a pepe"`)
-    } else if (object.Body.toLowerCase().indexOf("reminder") >= 0) {
+const handler = async (event, context) => {
+  // module.exports.handler = async (event, context) => {
+  // console.log(JSON.stringify(event))
+  try {
+    let object
+
+    if (['test', 'prod'].includes(process.env.NODE_ENV))
+      object = bodyParser(event.body)
+    else
+      object = {
+        From: process.env.TEST_PHONE_NUMBER,
+        Body: process.env.BODY_REQUEST,
+        To: "123"
+      }
+
+    let twiml
+    if (process.env.NODE_ENV == 'dev')
+      twiml = { message: response => console.log(response) }
+    else
+      twiml = new MessagingResponse();
+
+    if (object.Body.toLowerCase().indexOf("reminder") >= 0) {
       twiml.message("🍁🍁🍁🍁 \nReminder is not avilable yet, we're working hard with some drugs to give you this soon!! ")
+    } else if (object.Body.toLowerCase().indexOf("total+less") >= 0) {
+      console.log('here');
+
+      let response = await cashController.substractCategories(object)
+      twiml.message(response)
+    } else if (object.Body.toLowerCase().indexOf("total+sum") >= 0) {
+      let response = await cashController.sumCategories(object)
+      twiml.message(response)
     } else if (object.Body.toLowerCase().indexOf("total") >= 0) {
       let response = await cashController.listCategory(object)
+      twiml.message(response)
+    } else if (object.Body.toLowerCase().indexOf("cash+update") >= 0) {
+      let response = await cashController.updateCategory(object)
       twiml.message(response)
     } else if (object.Body.toLowerCase().indexOf("cash") >= 0) {
       let response = await cashController.addRecord(object)
@@ -35,3 +58,5 @@ module.exports.handler = async (event, context) => {
   }
 
 };
+
+handler({}, { done: () => process.exit(0) })
--- POST-whatsapp/src/controllers/finanzasController.js
+++ POST-whatsapp/src/controllers/finanzasController.js
@@ -3,6 +3,42 @@ const boxflowModel = require('../models/boxflow')
 const moment = require('moment')
 
 module.exports = {
+    updateCategory: async (request) => {
+        try {
+            await mongodb.connect()
+        } catch (error) {
+            console.error('error trying to connect with mongo' + error)
+            return "Error trying to connect with database"
+        }
+
+        let { From, To, Body } = request
+        if (!From || !To, !Body) return "Internal Error :trollface:"
+
+        let [task, auxTask, categoryFrom, categoryTo] = Body.split("+")
+        console.log(task, auxTask, categoryFrom, categoryTo);
+
+        if (!categoryFrom || !categoryTo)
+            return "CategoryFrom or categoryTo wasn't sent"
+
+        let query = {
+            phoneNumber: From,
+            category: categoryFrom,
+        }
+
+        let update = {
+            category: categoryTo
+        }
+
+        try {
+            let result = await boxflowModel.update(query, update)
+            console.log(JSON.stringify(result))
+            // await mongodb.destroy()
+        } catch (error) {
+            console.log(error)
+            return "Something saving in database"
+        }
+        return "Ok, i've update all purchases"
+    },
     addRecord: async (request) => {
         try {
             await mongodb.connect()
@@ -16,7 +52,10 @@ module.exports = {
 
         let [task, category, amount, ...description] = Body.split("+")
         description = description.join(" ")
+
         console.log(task, category, amount, description);
+
+        if (!category || !amount) return "Capa 8 \nYou should send: Cash <Category> <Amount> <Description?> "
 
         category = category ? category.split("/") : []
 
@@ -49,20 +88,26 @@ module.exports = {
         let { From, To, Body } = request
         if (!From || !To, !Body) return "Ey! Some params are missing"
 
-        let [task, category, timeAgo] = Body.split("+")
+        let [task, category, timeNumber, long] = Body.split("+")
 
-        console.log(task, category, timeAgo);
-        let timeNumber = timeAgo.match(/[0-100]/)[0]
-        let long = timeAgo.match(/[a-zA-Z]/)[0]
+        console.log(task, category, timeNumber, long);
 
-        if (!timeNumber || !long)
-            return "Error de capa 8 - Timming ago fails"
-        let date = moment().subtract(timeNumber, long)
+        if (!timeNumber || !long || !category)
+            return "Timming ago fails"
 
-        console.log(timeNumber, long, date)
+        if (category == 'help')
+            return "Run this command using: \n ``` Cash <Category> <#> <Days|weeks|month> ```"
+        const fromAux = category
+        if (category.indexOf('/') >= 0)
+            category = category.split('/').map(cat => new RegExp(cat, 'i'))
+        else
+            category = [new RegExp(category, 'i')]
+
+        let date = moment().subtract(timeNumber.toLowerCase(), long.toLowerCase())
+
         let query = {
             phoneNumber: From,
-            category,
+            category: { $in: category },
             createdDate: {
                 $gte: new Date(date)
             }
@@ -74,7 +119,7 @@ module.exports = {
             if (!result || !result.length)
                 return "⚠ No encontré datos 🙅‍♀️"
 
-            let data = "👽 Resumen de " + category + "\n"
+            let data = "👽 Resumen de " + fromAux + "\n"
             data += result.reduce((prev, current, idx) => {
                 prev += `${idx + 1}-${current.description}: $${current.amount}\n`
                 return prev
@@ -86,6 +131,127 @@ module.exports = {
             }, 0)
 
             return data
+        } catch (error) {
+            console.log(error)
+            return "Something fails with mongo"
+        }
+    },
+    sumCategories: async (request) => {
+        try {
+            await mongodb.connect()
+        } catch (error) {
+            console.error('error trying to connect with mongo' + error)
+            return "Error trying to connect with database"
+        }
+
+        let { From, To, Body } = request
+        if (!From || !To, !Body) return "Ey! Some params are missing"
+
+        let [task, subtask, category, timeNumber, long] = Body.split("+")
+        console.log(task, subtask, category, timeNumber, long);
+
+        if (!timeNumber || !long || !category)
+            return "Timming ago fails"
+
+        if (category == 'help')
+            return "Run this command using: \n ``` Total Sum <Category/Category/...> <#> <Days|weeks|month> ```"
+
+        const fromAux = category
+        if (category.indexOf('/') >= 0)
+            category = category.split('/').map(cat => new RegExp(cat, 'i'))
+        else
+            category = [new RegExp(category, 'i')]
+
+        let date = moment().subtract(timeNumber.toLowerCase(), long.toLowerCase())
+
+        let query = {
+            phoneNumber: From,
+            category: { $in: category },
+            createdDate: {
+                $gte: new Date(date)
+            }
+        }
+
+        try {
+            // return JSON.stringify(query)
+            let result = await boxflowModel.find(query, { description: 1, amount: 1, category: 1 })
+            if (!result || !result.length)
+                return "⚠ No encontré datos 🙅‍♀️"
+
+            let data = `👽 Suma Total de ${fromAux} +\n`
+            data += "*Total:* $" + result.reduce((prev, current, index) => {
+                prev += current.amount
+                return prev
+            }, 0)
+
+            return data
+        } catch (error) {
+            console.log(error)
+            return "Something fails with mongo"
+        }
+    },
+    substractCategories: async (request) => {
+        try {
+            await mongodb.connect()
+        } catch (error) {
+            console.error('error trying to connect with mongo' + error)
+            return "Error trying to connect with database"
+        }
+
+        let { From, To, Body } = request
+        if (!From || !To, !Body) return "Ey! Some params are missing"
+
+        let [task, subtask, toCategory, fromCategory, timeNumber, long] = Body.split("+")
+        console.log(task, subtask, fromCategory, toCategory, timeNumber, long);
+
+        if (!timeNumber || !long || !fromCategory || !toCategory)
+            return "Run this command using: \n ``` Total less <toCategory> <fromCategory/fromCategory/...> <#> <Days|weeks|month> ```"
+
+        const fromAux = fromCategory
+        const toAux = toCategory
+        if (fromCategory.indexOf('/') >= 0)
+            fromCategory = [...fromCategory.split('/').map(cat => new RegExp(cat, 'i')), new RegExp(toCategory, 'i')]
+        else
+            fromCategory = [new RegExp(fromCategory, 'i'), new RegExp(toCategory, 'i')]
+
+        let date = moment().subtract(timeNumber.toLowerCase(), long.toLowerCase())
+
+        let query = {
+            phoneNumber: From,
+            category: { $in: fromCategory },
+            createdDate: {
+                $gte: new Date(date)
+            }
+        }
+
+        try {
+            let result = await boxflowModel.find(query, { description: 1, amount: 1, category: 1 })
+            if (!result || !result.length)
+                return "⚠ No encontré datos 🙅‍♀️"
+
+            let data = `👽 Diferencia de ${toAux} & ${fromAux} +\n`
+
+            toCategory = result.filter(item => item.category.toLowerCase() == toCategory.toLowerCase())
+
+            if (!toCategory.length) return "Some categories were not found"
+
+            let fromCategory = result.reduce((prev, current) => {
+                console.log(current.category);
+
+                if (current.category !== toAux)
+                    prev += current.amount
+                return prev
+            }, 0)
+
+            const toCategoryTotal = toCategory.reduce((prev, current) => {
+                prev += current.amount
+                return prev
+            }, 0)
+            console.log(toCategoryTotal, fromCategory);
+
+            data += "fromAux: $" + toCategoryTotal
+            let a = data + "\nResult $" + (parseInt(fromCategory, 10) - parseInt(toCategoryTotal, 10))
+            return a
         } catch (error) {
             console.log(error)
             return "Something fails with mongo"
